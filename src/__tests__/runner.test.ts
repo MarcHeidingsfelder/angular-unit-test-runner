943d882f17e3e9403e697f2a75184c9418d3b42f
Marc Heidingsfelder <sorsan@arcor.de>
2022-09-20 12:09:19 +0200

    static read of angular config

--- src/__tests__/runner.test.ts
+++ src/__tests__/runner.test.ts
@@ -1,5 +1,24 @@
-import { Runner } from '../index';
+import { readFile } from "fs/promises";
+
+import { Runner, loadAngularConfig } from '../index';
 
 test('Runner', () => {
   expect(Runner('sorsan')).toBe('run tests for project sorsan');
+});
+
+
+jest.mock('fs/promises');//.createMockFromModule('fs');
+// jest.mock('readFile');
+const readFileMock = readFile as jest.MockedFunction<typeof readFile>;
+
+describe('load angular config', () => {
+  
+  it('load angular.json', async() => {
+    readFileMock.mockResolvedValue('{"projects": ["project1", "project2", "project3"]}');
+    const retVal = await loadAngularConfig();
+    expect(retVal)
+      .toEqual({ "projects": ["project1", "project2", "project3"] });
+  });
+
+
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const Runner = (project: string) => `run tests for project ${project}`;
+import { readFile } from "fs/promises";
+
+export const Runner = (project: string):string => `run tests for project ${project}`;
+
+export const loadAngularConfig = async ():Promise<AngularConfig> => {
+    try {
+        const configJson = await readFile('./angular.json', { encoding: 'utf-8' });
+        return JSON.parse(configJson);
+    } catch (error) {
+        return { projects: {} };
+    }
+
+} 
+
+declare type AngularConfig = { [keys: string]: unknown };
