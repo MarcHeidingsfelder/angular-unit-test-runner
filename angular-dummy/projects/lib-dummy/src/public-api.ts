23c95b124193cf0e30b092d967ff2a187390ce1e
MarcHeidingsfelder <marc.heidingsfelder@gmail.com>
2023-02-01 23:00:39 +0100

    remove angular dummy project

--- angular-dummy/projects/lib-dummy/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of lib-dummy
- */
-
-export * from './lib/lib-dummy.service';
-export * from './lib/lib-dummy.component';
-export * from './lib/lib-dummy.module';
--- src/mapper.ts
+++
@@ -1,9 +0,0 @@
-import { Project } from './types';
-
-export const extractProjectNames = (projects: Project[]): string[] => {
-  try {
-    return projects.map((project) => project.name);
-  } catch {
-    return [];
-  }
-};
