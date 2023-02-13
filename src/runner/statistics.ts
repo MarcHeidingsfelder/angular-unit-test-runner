ca3a8cb882064cf898991b51c7b164c61403837c
MarcHeidingsfelder <marc.heidingsfelder@gmail.com>
2023-02-13 05:50:05 +0100

    refactor(statistics) add constant for total property name

--- src/runner/statistics.ts
+++ src/runner/statistics.ts
@@ -8,27 +8,29 @@ export const calcStatistics= () => {
     let sumNetTime = 0;
   
     const summary: { [keys: string]: any } = {};
+    const summaryKey = 'total';
   
     console.log('###################### results #####################');
     console.log("got", AutState.resultCount(), "results");
   
     const results = AutState.getResults();
     for (const [key, value] of results) {
-      const { total, success, skipped, failed } = value;
-      const netTime = value.netTime / 1000;
-      summary[key] = { total, success, skipped, failed, netTime };
-      sumTotal += total;
-      sumSuccess += success;
-      sumSkipped += skipped;
-      sumFailed += failed;
-      sumNetTime += netTime;
-    }
-    summary['total'] = {
-      total: sumTotal,
-      success: sumSuccess,
-      skipped: sumSkipped,
-      failed: sumFailed,
-      netTime: sumNetTime
+        const { total, success, skipped, failed } = value;
+        const netTime = value.netTime / 1000;
+        summary[key] = { total, success, skipped, failed, netTime };
+        sumTotal += total;
+        sumSuccess += success;
+        sumSkipped += skipped;
+        sumFailed += failed;
+        sumNetTime += netTime;
+    }  
+    
+    summary[summaryKey] = {
+        total: sumTotal,
+        success: sumSuccess,
+        skipped: sumSkipped,
+        failed: sumFailed,
+        netTime: sumNetTime
     };
   
     console.log(summary);
